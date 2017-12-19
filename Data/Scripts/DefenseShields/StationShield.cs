e5a1f0d176700a540b907313700b4ec2b452a75c
shane <shane@geeklords.org>
2017-12-19 00:55:00 -0800

    auto

--- Data/Scripts/DefenseShields/StationShield.cs
+++ Data/Scripts/DefenseShields/StationShield.cs
@@ -668,14 +668,14 @@ namespace DefenseShields.Station
                         if (grid == _tblock.CubeGrid || _inHash.Contains(grid)) return;
                         
                         List<long> owners = grid.BigOwners;
-                        /*
-                        if (owners.Count > 0)
+                        
+                        if (owners.Count.Equals(1))
                         {
                             Logging.writeLine(String.Format("{0} - test", DateTime.Now));
                             var relations = _tblock.GetUserRelationToOwner(owners.Last());
                             if (relations == MyRelationsBetweenPlayerAndBlock.Owner || relations == MyRelationsBetweenPlayerAndBlock.FactionShare) return;
-                        }*/
-                        Logging.writeLine(String.Format("{0} - gridEffect: relations count for grid {1} was {2} with count {3} in loop {4}", DateTime.Now, grid, owners.Count, _count));
+                        }
+                        //Logging.writeLine(String.Format("{0} - gridEffect: relations count for grid {1} was {2} with count {3} in loop {4}", DateTime.Now, grid, owners.Count, _count));
                         List<IMySlimBlock> blockList = new List<IMySlimBlock>();
                         grid.GetBlocks(blockList);
                         foreach (var block in blockList)
