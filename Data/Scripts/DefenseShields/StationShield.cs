e42e9a46d052dfb1cc41f2d57f47716a7368f397
shane <shane@geeklords.org>
2017-12-19 22:52:56 -0800

    auto

--- Data/Scripts/DefenseShields/StationShield.cs
+++ Data/Scripts/DefenseShields/StationShield.cs
@@ -166,6 +166,7 @@ namespace DefenseShields.Station
                 Initialized = false;
 
             }
+            Logging.writeLine(String.Format("{0} - Updating CustomInfo {1}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), _count));
             ((IMyFunctionalBlock)_cblock).AppendingCustomInfo += AppendingCustomInfo;
             _tblock.RefreshCustomInfo();
             ((IMyFunctionalBlock)_cblock).AppendingCustomInfo += AppendingCustomInfo;
