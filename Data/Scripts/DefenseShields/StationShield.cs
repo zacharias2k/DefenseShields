85cedb2214e8bc1a3ba082a68d6fdbb57ae08eb8
shane <shane@geeklords.org>
2017-12-23 04:11:30 -0800

    auto

--- Data/Scripts/DefenseShields/StationShield.cs
+++ Data/Scripts/DefenseShields/StationShield.cs
@@ -473,8 +473,8 @@ namespace DefenseShields.Station
                 _inList = MyAPIGateway.Entities.GetTopMostEntitiesInSphere(ref insphere);
                 MyAPIGateway.Parallel.ForEach(_inList, outent =>
                 {
-                    if (outent is MyMeteor) return;
-                    if (outent.ToString().Contains("MyMeteor") || outent is IMyMeteor) return;
+                    //if (outent is MyMeteor) return;
+                    if (outent is IMyMeteor || outent.ToString().Contains("MyMeteor")) return;
                     if (Detectin(outent))
                     {
                         if (!_inList.Contains(outent)) _inList.Add(outent);
@@ -490,7 +490,7 @@ namespace DefenseShields.Station
                 if (_insideReady == false) Logging.WriteLine(String.Format("{0} - HOW CAN THIS BE! -Count: {1}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), Count));
                 if (webent == null) return;
                 if (webent is IMyMeteor  && !_shotwebbed) _shotwebbed = true;
-                if (webent is MyMeteor || !Detectout(webent)) return;
+                if (webent is IMyMeteor || !Detectout(webent)) return;
                 if (webent is IMyCharacter && Count == 14 || Count == 29 || Count == 44 || Count == 59)
                 {
                     var dude = MyAPIGateway.Players.GetPlayerControllingEntity(webent).IdentityId;
