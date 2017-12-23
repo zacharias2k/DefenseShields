d08af809dc14aab605eabae7e63f4205fdc6d2c4
shane <shane@geeklords.org>
2017-12-22 18:13:48 -0800

    auto

--- Data/Scripts/DefenseShields/StationShield.cs
+++ Data/Scripts/DefenseShields/StationShield.cs
@@ -437,7 +437,7 @@ namespace DefenseShields.Station
             float detect = (x * x) / ((_width - 13.3f) * (_width - 13.3f)) + (y * y) / ((_depth - 13.3f) * (_depth - 13.3f)) + (z * z) / ((_height - 13.3f) * (_height - 13.3f));
             if (detect > 1)
             {
-                Logging.WriteLine(String.Format("{0} - {1} beyond-i-boundary cords: x:{2} y:{3} z:{4} d:{5} l:{6}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), ent, x, y, z, detect, Count));
+                if (detect > 8 || detect < 2) Logging.WriteLine(String.Format("{0} - {1} beyond-o-boundary cords: x:{2} y:{3} z:{4} d:{5} l:{6}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), ent, x, y, z, detect, Count));
                 return false;
             }
             return true;
