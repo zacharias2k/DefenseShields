1ebcb016c911704f788635b263a55e38b852ad60
shane <shane@geeklords.org>
2017-12-22 23:34:29 -0800

    auto

--- Data/Scripts/DefenseShields/StationShield.cs
+++ Data/Scripts/DefenseShields/StationShield.cs
@@ -441,7 +441,7 @@ namespace DefenseShields.Station
             float detect = (x * x) / (_width -13.3f * _width - 13.3f) + (y * y) / (_depth - 13.3f * _depth - 13.3f) + (z * z) / (_height - 13.3f * _height - 13.3f);
             if (detect > 1)
             {
-                Logging.WriteLine(String.Format("{0} - {1} within-t-boundary cords: {2} {3} {4} {5}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), ent, x, y, z, detect));
+                Logging.WriteLine(String.Format("{0} - {1} within-t: x:{2} y:{3} z:{4} d:{5}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), ent, x, y, z, detect));
                 return false;
             }
             //Logging.WriteLine(String.Format("{0} - {1} within-f-boundary cords: {2} {3} {4} {5}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), ent, x, y, z, detect));
@@ -460,10 +460,10 @@ namespace DefenseShields.Station
             float detect = (x * x) / (_width * _width) + (y * y) / (_depth * _depth) + (z * z) / (_height * _height);
             if (detect > 1)
             {
-                if (detect >8 || detect >1.2) Logging.WriteLine(String.Format("{0} - {1} beyond-t-boundary cords: {2} {3} {4} {5}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), ent, x, y, z, detect));
+                if (detect >8 || detect >1.2) Logging.WriteLine(String.Format("{0} - {1} beyond-t : x:{2} y:{3} z:{4} d:{5}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), ent, x, y, z, detect));
                 return false;
             }
-            Logging.WriteLine(String.Format("{0} - {1} beyond-f-boundary cords: {2} {3} {4} {5}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), ent, x, y, z, detect));
+            Logging.WriteLine(String.Format("{0} - {1} beyond-f: {2} {3} {4} {5}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), ent, x, y, z, detect));
             return true;
         }
         #endregion
