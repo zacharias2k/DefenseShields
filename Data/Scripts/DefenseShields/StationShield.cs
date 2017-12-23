732f1ba43d48790051d0236a1f8c13f5cd03e533
shane <shane@geeklords.org>
2017-12-23 03:52:42 -0800

    auto

--- Data/Scripts/DefenseShields/StationShield.cs
+++ Data/Scripts/DefenseShields/StationShield.cs
@@ -488,9 +488,9 @@ namespace DefenseShields.Station
             MyAPIGateway.Parallel.ForEach(webList, webent =>
                 {
                 if (_insideReady == false) Logging.WriteLine(String.Format("{0} - HOW CAN THIS BE! -Count: {1}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), Count));
-                if (webent == null || webent.ToString().Contains("MyMeteor") || webent is MyMeteor || webent is IMyMeteor || !Detectout(webent)) return;
                 if (webent == null) return;
                 if (webent is IMyMeteor  && !_shotwebbed) _shotwebbed = true;
+                if (webent is IMyMeteor || !Detectout(webent)) return;
                 if (webent is IMyCharacter && Count == 14 || Count == 29 || Count == 44 || Count == 59)
                 {
                     var dude = MyAPIGateway.Players.GetPlayerControllingEntity(webent).IdentityId;
