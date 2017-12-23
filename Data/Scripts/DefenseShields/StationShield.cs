4ea79b6b988881395aa15f3d9463f203a0ce7903
shane <shane@geeklords.org>
2017-12-22 22:50:15 -0800

    auto

--- Data/Scripts/DefenseShields/StationShield.cs
+++ Data/Scripts/DefenseShields/StationShield.cs
@@ -411,7 +411,7 @@ namespace DefenseShields.Station
         public void ShowRange(float size)
         {
             // Draw shield
-            MyStringId RangeGridResourceId = MyStringId.GetOrCompute("Build new");
+            //MyStringId RangeGridResourceId = MyStringId.GetOrCompute("Build new");
             Color colour;
             var relations = _tblock.GetUserRelationToOwner(MyAPIGateway.Session.Player.IdentityId);
             if (relations == MyRelationsBetweenPlayerAndBlock.Owner || relations == MyRelationsBetweenPlayerAndBlock.FactionShare)
@@ -420,8 +420,8 @@ namespace DefenseShields.Station
                 colour = Color.FromNonPremultiplied(255 - _colourRand, 80 + _colourRand, 16, 72);
             //MatrixD matrix = MatrixD.CreateFromTransformScale(Quaternion.CreateFromRotationMatrix(_worldMatrix.GetOrientation()), _worldMatrix.Translation, _scale);
             var m = MatrixD.Rescale(_worldMatrix, new Vector3D(_width, _height, _depth));
-            var c = Color.Blue * 0.5f;
-            MySimpleObjectDraw.DrawTransparentSphere(ref m, _range, ref c, MySimpleObjectRasterizer.Solid, 24, MyStringId.GetOrCompute("Square"));
+            //var c = Color.Blue * 0.5f;
+            MySimpleObjectDraw.DrawTransparentSphere(ref m, 1f, ref colour, MySimpleObjectRasterizer.Solid, 20, null, MyStringId.GetOrCompute("Square"));
             //MySimpleObjectDraw.DrawTransparentSphere(ref matrix, _range, ref colour, MySimpleObjectRasterizer.Solid, 20, null, RangeGridResourceId, 0.25f, -1);
             // end shield draw
         }
