9e43355068c6e3d5b9eb045fecb2760ddf651b1a
shane <shane@geeklords.org>
2017-12-22 23:19:54 -0800

    auto

--- Data/Scripts/DefenseShields/StationShield.cs
+++ Data/Scripts/DefenseShields/StationShield.cs
@@ -441,7 +441,7 @@ namespace DefenseShields.Station
             float detect = (x * x) / (_width -13.3f * _width - 13.3f) + (y * y) / (_depth - 13.3f * _depth - 13.3f) + (z * z) / (_height - 13.3f * _height - 13.3f);
             if (detect > 1)
             {
-                if (detect > 8 || detect < 1) Logging.WriteLine(String.Format("{0} - {1} beyond-i-boundary cords: {2} {3} {4} {5}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), ent, x, y, z, detect));
+                Logging.WriteLine(String.Format("{0} - {1} beyond-i-boundary cords: {2} {3} {4} {5}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), ent, x, y, z, detect));
                 return false;
             }
             return true;
@@ -459,7 +459,7 @@ namespace DefenseShields.Station
             float detect = (x * x) / (_width * _width) + (y * y) / (_depth * _depth) + (z * z) / (_height * _height);
             if (detect > 1)
             {
-                if (detect > 8 || detect < 1) Logging.WriteLine(String.Format("{0} - {1} beyond-o-boundary cords: {2} {3} {4} {5}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), ent, x, y, z, detect));
+                Logging.WriteLine(String.Format("{0} - {1} beyond-o-boundary cords: {2} {3} {4} {5}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), ent, x, y, z, detect));
                 return false;
             }
             return true;
@@ -506,7 +506,7 @@ namespace DefenseShields.Station
                         if (relationship != MyRelationsBetweenPlayerAndBlock.Owner &&
                             relationship != MyRelationsBetweenPlayerAndBlock.FactionShare)
                         {
-                            _playerwebbed = true;
+                            //_playerwebbed = true;
                             return;
                         }
                         return;
