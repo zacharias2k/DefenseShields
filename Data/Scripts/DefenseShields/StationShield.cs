7954841d2ec0ae1c3d1c8526ada034251317dffc
shane <shane@geeklords.org>
2017-12-22 21:35:12 -0800

    auto

--- Data/Scripts/DefenseShields/StationShield.cs
+++ Data/Scripts/DefenseShields/StationShield.cs
@@ -48,7 +48,8 @@ namespace DefenseShields.Station
         private ushort _modId = 50099;
 
         private static Random _random = new Random();
-        private MatrixD _worldMatrix;
+        //private MatrixD _worldMatrix;
+        private MatrixD _worldMatrix = MatrixD.Identity;
         private MatrixD _worldMatrix2;
         private Vector3D _scale;
         private BoundingSphereD _sphereMin;
@@ -429,9 +430,9 @@ namespace DefenseShields.Station
         #region Detect innersphere intersection
         private bool Detectin(IMyEntity ent)
         {
-            float x = Vector3Extensions.Project(_worldMatrix2.Forward, ent.GetPosition() - _worldMatrix2.Translation).AbsMax();
-            float y = Vector3Extensions.Project(_worldMatrix2.Left, ent.GetPosition() - _worldMatrix2.Translation).AbsMax();
-            float z = Vector3Extensions.Project(_worldMatrix2.Up, ent.GetPosition() - _worldMatrix2.Translation).AbsMax();
+            float x = Vector3Extensions.Project(_worldMatrix.Forward, ent.GetPosition() - _worldMatrix.Translation).AbsMax();
+            float y = Vector3Extensions.Project(_worldMatrix.Left, ent.GetPosition() - _worldMatrix.Translation).AbsMax();
+            float z = Vector3Extensions.Project(_worldMatrix.Up, ent.GetPosition() - _worldMatrix.Translation).AbsMax();
             float detect = (x * x) / (_width -13.3f * _width - 13.3f) + (y * y) / (_depth - 13.3f * _depth - 13.3f) + (z * z) / (_height - 13.3f * _height - 13.3f);
             if (detect > 1)
             {
