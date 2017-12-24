70a23795ea6f905c25983c59a0383de1067cf0ea
shane <shane@geeklords.org>
2017-12-24 13:26:49 -0800

    auto

--- Data/Scripts/DefenseShields/StationShield.cs
+++ Data/Scripts/DefenseShields/StationShield.cs
@@ -510,15 +510,14 @@ namespace DefenseShields.Station
             MyAPIGateway.Parallel.ForEach(_inList, outent =>
             {
                 if (outent is IMyMeteor || outent.ToString().Contains("MyMeteor")) return;
-                if (outent is IMyCubeGrid)
+                if (outent is IMyCubeGrid grid)
                 {
-                    var grid = outent as IMyCubeGrid;
                     if (grid == _tblock.CubeGrid) return;
                     double abs = Math.Abs(grid.WorldAABB.HalfExtents.Dot(grid.WorldAABB.Max - insphere.Center) * 2);
                     if (Detectgridedge(grid, abs))
+                        Logging.WriteLine(String.Format("{0} - begin Count: {1}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), Count));
                         if (!_inList.Contains(grid))
                         {
-                        Logging.WriteLine(String.Format("{0} - begin Count: {1}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), Count));
                         Logging.WriteLine(String.Format("{0} - {1} added to inside sphere: {2}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), grid.CustomName, Count));
                             _inList.Add(grid);
                             return;
