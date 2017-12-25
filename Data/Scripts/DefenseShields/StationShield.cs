62406cfde10adf4cede85ebc911c4a4fa3c38ca5
shane <shane@geeklords.org>
2017-12-25 01:34:20 -0800

    auto

--- Data/Scripts/DefenseShields/StationShield.cs
+++ Data/Scripts/DefenseShields/StationShield.cs
@@ -569,17 +569,17 @@ namespace DefenseShields.Station
                 if (grid == _tblock.CubeGrid || _gridwebbed) return;
                 if (grid != null)
                 {
+                    List<long> owners = grid.BigOwners;
+                    if (owners.Count > 0)
+                    {
+                        var relations = _tblock.GetUserRelationToOwner(owners[0]);
+                        Logging.WriteLine(String.Format("{0} - grid: {1} tblock: {2} {3} {4} {5}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), grid.CustomName, owners.Count, relations, relations == MyRelationsBetweenPlayerAndBlock.Owner, relations == MyRelationsBetweenPlayerAndBlock.FactionShare));
+                        if (relations == MyRelationsBetweenPlayerAndBlock.Owner || relations == MyRelationsBetweenPlayerAndBlock.FactionShare) return;
+                    }
                     //double abs = Math.Abs(grid.WorldAABB.HalfExtents.Dot(grid.WorldAABB.Center - websphere.Center) * 2);
                     double abs = Math.Abs(grid.WorldAABB.HalfExtents.Dot(grid.WorldAABB.Max - websphere.Center) * 2);
                     if (Detectgridedge(grid, abs))
                     {
-                        List<long> owners = grid.BigOwners;
-                        if (owners.Count > 0)
-                        {
-                            var relations = _tblock.GetUserRelationToOwner(owners[0]);
-                            //Logging.WriteLine(String.Format("{0} - grid: {1} tblock: {2} {3} {4} {5}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), grid.CustomName, owners.Count, relations, relations == MyRelationsBetweenPlayerAndBlock.Owner, relations == MyRelationsBetweenPlayerAndBlock.FactionShare));
-                            if (relations == MyRelationsBetweenPlayerAndBlock.Owner || relations == MyRelationsBetweenPlayerAndBlock.FactionShare) return;
-                        }
                         Logging.WriteLine(String.Format("{0} - webEffect-grid: pass grid: {1}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), grid.CustomName));
                         _gridwebbed = true;
                         return;
