316008ba1a6ea0c2b1fe6ebc29150c4b1585d3c5
shane <shane@geeklords.org>
2017-12-25 13:18:40 -0800

    auto

--- Data/Scripts/DefenseShields/StationShield.cs
+++ Data/Scripts/DefenseShields/StationShield.cs
@@ -121,8 +121,8 @@ namespace DefenseShields.Station
                     Logging.WriteLine(String.Format("{0} - Resetting BlockAnimation in loop {1}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), Count));
                     _subpartRotor.Subparts.Clear();
                     _subpartsArms.Clear();
-                    Entity.TryGetSubpart("Rotor", out _subpartRotor);
-                    BlockAnimation();
+                    //Entity.TryGetSubpart("Rotor", out _subpartRotor);
+                    //BlockAnimation();
                 }
 
                 //if (Entity.TryGetSubpart("Rotor", out _subpartRotor))
