f317674c0d8865ccbeed28326014cd29afc20ea8
shane <shane@geeklords.org>
2017-12-25 16:44:58 -0800

    auto

--- Data/Scripts/DefenseShields/StationShield.cs
+++ Data/Scripts/DefenseShields/StationShield.cs
@@ -318,7 +318,7 @@ namespace DefenseShields.Station
             {
                 //var radius = Slider.Getter((IMyFunctionalBlock)_cblock);
                 //power = (float)(4.0 * Math.PI * Math.Pow(radius, 3) / 3.0 / 1000.0 / 1000.0);
-                if (Absorb < 1)
+                if (Absorb > 1)
                 {
                     Logging.WriteLine(String.Format("{0} - Absorb is {1}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), Absorb));
                     power += (Absorb / 60f);
