a75c56c6d3b0b70f5fba7d8361e025a4eff2081f
shane <shane@geeklords.org>
2017-12-25 16:35:10 -0800

    auto

--- Data/Scripts/DefenseShields/StationShield.cs
+++ Data/Scripts/DefenseShields/StationShield.cs
@@ -315,9 +315,18 @@ namespace DefenseShields.Station
             float power = 0.0001f;
             if (!Initialized && _cblock.IsWorking)
             {
-                var radius = Slider.Getter((IMyFunctionalBlock)_cblock);
-                power = (float)(4.0 * Math.PI * Math.Pow(radius, 3) / 3.0 / 1000.0 / 1000.0);
+                //ar radius = Slider.Getter((IMyFunctionalBlock)_cblock);
+                //power = (float)(4.0 * Math.PI * Math.Pow(radius, 3) / 3.0 / 1000.0 / 1000.0);
+                if (Count == 29)
+                {
+                    if (Absorb < 1)
+                    {
+                        Logging.WriteLine(String.Format("{0} - Absorb is {1}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), Absorb));
+                        power -= (Absorb / 60f);
+                    }
+                }
             }
+            Logging.WriteLine(String.Format("{0} - Power sinking is {1}", DateTime.Now.ToString("MM-dd-yy_HH-mm-ss-fff"), power));
             return power;
         }
 
