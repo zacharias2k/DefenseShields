ae546a4d6334fefcc986af9f6974eb2ae0e57c28
shane <shane@geeklords.org>
2019-02-02 09:36:06 -0800

    fds

--- Data/Scripts/DefenseShields/Session/SessionNetwork.cs
+++ Data/Scripts/DefenseShields/Session/SessionNetwork.cs
@@ -10,16 +10,18 @@
         #region Network sync
         internal void PacketizeToClientsInRange(IMyFunctionalBlock block, PacketBase packet)
         {
-            var bytes = MyAPIGateway.Utilities.SerializeToBinary(packet);
-            var localSteamId = MyAPIGateway.Multiplayer.MyId;
-
-            foreach (var p in Players.Values)
+            try
             {
-                var id = p.SteamUserId;
-
-                if (id != localSteamId && id != packet.SenderId && Vector3D.DistanceSquared(p.GetPosition(), block.PositionComp.WorldAABB.Center) <= SyncBufferedDistSqr)
-                    MyAPIGateway.Multiplayer.SendMessageTo(PACKET_ID, bytes, p.SteamUserId);
+                var bytes = MyAPIGateway.Utilities.SerializeToBinary(packet);
+                var localSteamId = MyAPIGateway.Multiplayer.MyId;
+                foreach (var p in Players.Values)
+                {
+                    var id = p.SteamUserId;
+                    if (id != localSteamId && id != packet.SenderId && Vector3D.DistanceSquared(p.GetPosition(), block.PositionComp.WorldAABB.Center) <= SyncBufferedDistSqr)
+                        MyAPIGateway.Multiplayer.SendMessageTo(PACKET_ID, bytes, p.SteamUserId);
+                }
             }
+            catch (Exception ex) { Log.Line($"Exception in PacketizeToClientsInRange: {ex}"); }
         }
 
         private void ReceivedPacket(byte[] rawData)
